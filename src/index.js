7e0f1ca0dc168b5c64ff449225989e3492f1bb03
chanaka subasinghe <chanaka.s@icloud.com>
2020-12-24 17:04:24 +0530

    change index file

--- src/index.js
+++ src/index.js
@@ -29,11 +29,11 @@ app.use('/api/employees/', salaryRoute);
 
 if (environment === 'production') {
     app.use(express.static('client/build'));
-
-    app.get('*', async (req, res) => {
-        res.sendFile(path.join('client/build/index.html'));
-    });
 }
+
+app.get('*', async (req, res) => {
+    res.sendFile(path.join('client/build/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
