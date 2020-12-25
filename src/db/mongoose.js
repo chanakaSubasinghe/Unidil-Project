1ef38b07d90436f541de257356e8b77c648b60bc
chanaka subasinghe <chanaka.s@icloud.com>
2020-12-25 13:59:43 +0530

    modify mongoose file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,21 +1,8 @@
 // requiring mongoose
 const mongoose = require('mongoose');
 
-// DB connect function
-const connectDataBase = async (URL) => {
-
-    try {
-        // database connection
-        await mongoose.connect(URL, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-        }, () => console.log('Database Connected!'));
-    } catch (error) {
-        console.log('Error', error);
-    }
-
-};
-
-await connectDataBase(process.env.MONGODB_URL);
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
