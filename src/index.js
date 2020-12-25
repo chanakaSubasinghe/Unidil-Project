ebd2ea48a205f80eebfd876f8dc2e9dde56e0259
chanaka subasinghe <chanaka.s@icloud.com>
2020-12-25 13:03:03 +0530

    refactor index.js file

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const environment = process.env.NODE_ENV || 'development';
 
-app.get('/', (req, res) => {
-    res.json({ name: `port - ${process.env.PORT}, environment - ${process.env.NODE_ENV}` });
-});
+// app.get('/', (req, res) => {
+//     res.json({ name: `port - ${process.env.PORT}, environment - ${process.env.NODE_ENV}` });
+// });
 
 app.use(express.json());
 app.use(cors());
@@ -29,11 +29,11 @@ app.use('/api/employees/', salaryRoute);
 
 if (environment === 'production') {
     app.use(express.static('client/build'));
-}
 
-app.get('*', async (req, res) => {
-    res.sendFile(path.join('client/build/index.html'));
-});
+    app.get('*', async (req, res) => {
+        res.sendFile(path.join('client/build/index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
